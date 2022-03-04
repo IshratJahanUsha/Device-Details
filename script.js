2cec85259369dd1b4cbdd3dc8225542169320948
IshratJahanUsha <ishratjahanusha2021@gmail.com>
2022-03-04 20:57:10 +0600

    8i

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const searchDevice = () =>{
 const displaySearchResults = datas => {
     let searchResult = document.getElementById('search-result');
 
-    datas.forEach(eachData => {
+    datas.slice(0,20).forEach(eachData => {
 
         //making cards
         const div = document.createElement('div')
@@ -52,7 +52,7 @@ const displaySearchResults = datas => {
             <img src="${eachData.image}" alt="device">
             <h3 class="card-title">Model:${eachData.phone_name}</h3>
             <h4 class="card-text">Brand:${eachData.brand}</h4>
-            <button id="details-btn" class="btn" onclick = "loadDeviceDetails('${eachData.slug}')">Details</button>
+            <button id="details-btn" class="btn" onclick = "loadDeviceDetails('${eachData.slug}')"><a href="">Details</a></button>
         </div>`
 
         searchResult.appendChild(div);
@@ -82,34 +82,33 @@ const displayDeviceDetails = datas => {
         div.innerHTML = `
         <div class="div-1">
             <img src="${datas.image}" alt="device">
-            <h4 class="card-title">Model: ${datas.name}</h4>
-            <h4 class="card-text">Brand: ${datas.brand}</h4>
-            <h4 class="card-text">Release Date: ${datas.releaseDate != '' ? datas.releaseDate : 'No Release Date Found'}</h4>
+            <h3> <span class="span"> Model: </span> ${datas.name}</h3>
+            <h4> <span class="span"> Brand: </span> ${datas.brand}</h4>
+            <h4> <span class="span"> Release Date: </span> ${datas.releaseDate != '' ? datas.releaseDate : 'No Release Date Found'}</h4>
         </div>
         <div class="div-2">
-            <h3>Main Feature</h3>
-            <h4> <span> Display Size: <span> ${datas.mainFeatures.displaySize} </h4>
-            <h4> <span> ChipSet: <span> ${datas.mainFeatures.chipSet} </h4>
-            <h4> <span> Storage: <span> ${datas.mainFeatures.storage} </h4>
-            <h4> <span> Memory: <span> ${datas.mainFeatures.memory} </h4>
-            <h4> <span> Sensors: <span> ${datas.mainFeatures.sensors} </h4>
+            <h2>Main Feature</h2>
+            <h4> <span class="span"> Display Size: </span> ${datas.mainFeatures.displaySize} </h4>
+            <h4> <span class="span"> ChipSet: </span> ${datas.mainFeatures.chipSet} </h4>
+            <h4> <span class="span"> Storage: </span> ${datas.mainFeatures.storage} </h4>
+            <h4> <span class="span"> Memory: </span> ${datas.mainFeatures.memory} </h4>
+            <h4> <span class="span"> Sensors: </span> ${datas.mainFeatures.sensors} </h4>
         </div>
         <div class="div-3">
-            <h3>Others</h3>
-            <h4> <span> Bluetooth: <span> ${datas.others != undefined ? datas.others.Bluetooth : "No Data Found"} </h4>
-            <h4><span>WLAN: </span> ${datas.others != undefined ? datas.others.WLAN : "No Data Found"}</h4>
-            <h4> <span> GPS: <span> ${datas.others != undefined ? datas.others.GPS : "No Data Found"} </h4>
-            <h4> <span> NFC: <span> ${datas.others != undefined ? datas.others.NFC : "No Data Found"} </h4>
-            <h4> <span> Radio: <span>  ${datas.others != undefined ? datas.others.Radio : "No Data Found"} </h4>
-            <h4> <span> USB: <span> ${datas.others != undefined ? datas.others.USB : "No Data Found"} </h4>
+            <h2>Others</h2>
+            <h4> <span class="span"> Bluetooth: </span> ${datas.others != undefined ? datas.others.Bluetooth : "No Data Found"} </h4>
+
+            <h4> <span class="span">WLAN: </span> ${datas.others != undefined ? datas.others.WLAN : "No Data Found"}</h4>
+
+            <h4> <span class="span"> GPS: </span> ${datas.others != undefined ? datas.others.GPS : "No Data Found"} </h4>
+
+            <h4> <span class="span"> NFC: </span> ${datas.others != undefined ? datas.others.NFC : "No Data Found"} </h4>
+
+            <h4> <span class="span"> Radio: </span>  ${datas.others != undefined ? datas.others.Radio : "No Data Found"} </h4>
+
+            <h4> <span class="span"> USB: </span> ${datas.others != undefined ? datas.others.USB : "No Data Found"} </h4>
         </div>
        `
     deviceDetails.appendChild(div);
 
 }    
-
-
-
-// data.slice(0,20).
-
-
