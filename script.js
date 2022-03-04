dcdced30884649241283e9153c4b157b863f2bc6
IshratJahanUsha <ishratjahanusha2021@gmail.com>
2022-03-04 12:09:11 +0600

    3rd

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-// search button work
+//* search button work
 
 const searchDevice = () =>{
    const searchInput = document.getElementById('search-input');
    let searchText = searchInput.value;
+    // if search input empty   
    if(searchText == ''){
        let searchResult = document.getElementById('search-result');
-       searchResult.innerHTML =`<p class = "no-device-found">No Device Found</p>`
+       searchResult.innerHTML =`<p class = "no-result-found">No Device Found</p>`
        return searchResult;
-   }else{
-    let searchResult = document.getElementById('search-result');
-    searchResult.innerText = ""
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch (url)
-    .then(res => res.json())
-    .then(data => displaySearchResults(data.data))
    }
+    // if search input not empty   
+   else{
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+        .then(res1 => res1.json())
+        .then(data1 => checkData(data1.status))
+        const checkData = check =>{
+            // if data matched
+            if(check){
+                let searchResult = document.getElementById('search-result');
+                searchResult.innerHTML = ""
+                const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+                fetch (url)
+                .then(res => res.json())
+                .then(data => displaySearchResults(data.data))
+            }
+            // if data not matched
+            else{
+                let searchResult = document.getElementById('search-result');
+                searchResult.innerHTML =`
+                <h1 class="no-result-found">No Result Found</h1>` 
+                
+                return searchResult;
+            }
+        }
+    }
     searchInput.value ='' 
 }
 
+// display cards work
 const displaySearchResults = datas => {
-    // console.log(datas)
     let searchResult = document.getElementById('search-result');
+
     datas.forEach(eachData => {
-        // console.log(eachData.slug)
+
+        //making cards
         const div = document.createElement('div')
-        div.classList.add('device-cards');
+        div.classList.add('device-card');
         div.innerHTML = `
         <div>
             <img src="${eachData.image}" alt="device">
-            <h4 class="card-title">Model:${eachData.phone_name}</h4>
+            <h3 class="card-title">Model:${eachData.phone_name}</h3>
             <h4 class="card-text">Brand:${eachData.brand}</h4>
             <button id="details-btn" class="btn" onclick = "loadDeviceDetails('${eachData.slug}')">Details</button>
         </div>`
@@ -39,51 +61,55 @@ const displaySearchResults = datas => {
 
 }
 
-// detail button work
+//full detail section
 
+// Details button work
 const loadDeviceDetails = deviceId => {
-    const url = `https://openapi.programming-hero.com/api/phone/'${deviceId}'`
+    let url = `https://openapi.programming-hero.com/api/phone/${deviceId}`
     fetch (url)
     .then(res => res.json())
     .then(data => displayDeviceDetails(data.data))
-    // console.log(deviceId);
-    // console.log(url);
+    
 }
 
+// display detail work
 const displayDeviceDetails = datas => {
     let deviceDetails = document.getElementById('device-details');
-    datas.forEach(eachData => {
+    deviceDetails.textContent = ''
+    // full detail card
         const div = document.createElement('div')
         div.classList.add('device-details');
         div.innerHTML = `
-        <div>
-            <img src="${eachData.image}" alt="device">
-            <h4 class="card-title">Model:${eachData.phone_name}</h4>
-            <h4 class="card-text">Brand:${eachData.brand}</h4>
-            <h4 class="card-text">Release Date:${eachData.releaseDate}</h4>
+        <div class="div-1">
+            <img src="${datas.image}" alt="device">
+            <h4 class="card-title">Model: ${datas.name}</h4>
+            <h4 class="card-text">Brand: ${datas.brand}</h4>
+            <h4 class="card-text">Release Date: ${datas.releaseDate != '' ? datas.releaseDate : 'No Release Date Found'}</h4>
         </div>
-        <div>
-            <h4 class="card-mainFeature">Main Features:${eachData.mainFeatures}</h4>
-            <h4 class="card-sensors">Sensors:${eachData.sensors}</h4>
+        <div class="div-2">
+            <h3>Main Feature</h3>
+            <h4> <span> Display Size: <span> ${datas.mainFeatures.displaySize} </h4>
+            <h4> <span> ChipSet: <span> ${datas.mainFeatures.chipSet} </h4>
+            <h4> <span> Storage: <span> ${datas.mainFeatures.storage} </h4>
+            <h4> <span> Memory: <span> ${datas.mainFeatures.memory} </h4>
+            <h4> <span> Sensors: <span> ${datas.mainFeatures.sensors} </h4>
         </div>
-        <div>
-            <h4 class="card-others">Others:${eachData.others}</h4>
+        <div class="div-3">
+            <h3>Others</h3>
+            <h4> <span> Bluetooth: <span> ${datas.others != undefined ? datas.others.Bluetooth : "No Data Found"} </h4>
+            <h4><span>WLAN: </span> ${datas.others != undefined ? datas.others.WLAN : "No Data Found"}</h4>
+            <h4> <span> GPS: <span> ${datas.others != undefined ? datas.others.GPS : "No Data Found"} </h4>
+            <h4> <span> NFC: <span> ${datas.others != undefined ? datas.others.NFC : "No Data Found"} </h4>
+            <h4> <span> Radio: <span>  ${datas.others != undefined ? datas.others.Radio : "No Data Found"} </h4>
+            <h4> <span> USB: <span> ${datas.others != undefined ? datas.others.USB : "No Data Found"} </h4>
         </div>
        `
-        deviceDetails.appendChild(div);
-
-        // console.log(datas);
-        // console.log(eachData);
-        // console.log(div);
-
-    })
+    deviceDetails.appendChild(div);
 
 }    
 
-//url Formet:  https://openapi.programming-hero.com/api/phone/${id}
 
 
-
-
+// data.slice(0,20).
 
 
